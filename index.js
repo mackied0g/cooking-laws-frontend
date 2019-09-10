8336d9ac8fcb1f0883e46ace06ff95f24650c35e
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-09-10 13:26:53 -0400

    better, kinda

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // const loginForm = document.querySelector('#div')
 const ulLaws = document.querySelector("#ul-of-laws");
 
-
-
 document.addEventListener("DOMContentLoaded", function(){
 
 
@@ -14,75 +12,28 @@ fetch(`http://localhost:3000/laws`)
 // taking the fetch request and rendering it to the DOM
 function renderLaws(dataLaw) {
     // console.log(dataLaw) //each dataLaw has nested array of recipes
-    // debugger
        dataLaw.forEach(function(law) {
             const divTagForLaw = document.createElement("div");
             const br = document.createElement('br');
-            const li = document.createElement('li');
-            // console.log(divTagForLaw, "hello from line 22")
             divTagForLaw.innerText = law.name;
             const recipesForLaw = law.recipes
             console.log(recipesForLaw) //array of Recipe objects
-            // console.log(dataLaw)
 
             //nested forEach to iterate through Recipe objects
             recipesForLaw.forEach(singleRecipeObject => {
-                //I need to create an li for each recipesForLaw
-                li.innerText += singleRecipeObject.name
-                divTagForLaw.append(li)
+                const liTagForRecipe = document.createElement('li');
+                liTagForRecipe.innerText += singleRecipeObject.name
+                divTagForLaw.append(liTagForRecipe)
+                divTagForLaw.append(singleRecipeObject)
         })
-        // debugger
-        // console.log(law.name)
-        // console.log(recipesForLaw) //array of Recipe objects!
+        
         ulLaws.append(br)
         ulLaws.append(divTagForLaw)
-        divTagForLaw.append(recipesForLaw)
+        
         
         //create a function onClick to display recipes associated with each law
     });
 }
-
-// document.querySelector("#ul-of-laws").addEventListener("click", renderRecipes());
-
-// // now fetch the recipes
-// fetch(`http://localhost:3000/recipes`)
-//     .then(resp => resp.json())
-//     .then(dataRecipe => renderRecipes(dataRecipe))
-
-// // render the recipes fetched 
-// function renderRecipes(dataRecipe) {
-//     // console.log(dataRecipe)
-//     dataRecipe.forEach(function(recipe) {
-//         // console.log(recipe)
-//         const liRecipeName = document.createElement("li");
-//         const br = document.createElement('br');
-//         liRecipeName.innerHTML = recipe.name;
-//         console.log(liRecipeName, "hello from line 37")
-//         ulRecipes.append(liRecipeName)
-//         ulRecipes.append(br)
-//         ulRecipes.append(br)
-//     });
-// }
-
-// now fetch a specific recipe object
-// fetch(`http://localhost:3000/recipes/`) //add../id?
-//     .then(resp => resp.json())
-//     .then(dataOfOneRecipe => renderOneRecipe(dataOfOneRecipe))
-
-// // render the specific recipe object fetched 
-// function renderOneRecipe(dataOfOneRecipe) {
-//     dataOfOneRecipe.forEach(function(oneRecipe) {
-//         const liRecipeMoreInfo = document.createElement("li");
-//         const br = document.createElement('br');
-//         liRecipeMoreInfo.innerHTML = oneRecipe.description;
-//         console.log(liRecipeMoreInfo, "hello from line 57")
-//         ulRecipes.append(liRecipeMoreInfo)
-//         ulRecipes.append(br)
-//     });
-// }
-
-
-// ulLaws.addEventListener("click", function(event){ //ul is stable parent
 
 
 
