dcd2bc9a21acac7aaa8c7a6552831fa18e54c047
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-09-10 14:53:54 -0400

    fixed errors and added showing recipes from clicking on law

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
 // const loginForm = document.querySelector('#div')
 const ulLaws = document.querySelector(".ul-of-laws");
+document.addEventListener("click", () => {
+    handleClicks()
+})
+
+function handleClicks() {
+    // debugger
+    if (event.target.classList == "law" && event.target.id) {
+        let lawId = event.target.id)
+        // const recipesForLaw = law.recipes
+        lawRecipeFetch(lawId)
+    }
+    else if (event.target.id && event.target.classList == "recipe") { 
+        debugger
+    }
+}
+// document.addEventListener("DOMContentLoaded", function(){
+
+
+function lawRecipeFetch(lawId){
+    fetch(`http://localhost:3000/laws/${lawId}`)
+            .then(resp => resp.json())
+            .then(law => {
+                law.recipes.forEach(singleRecipeObject => {
+                    const liTagForRecipe = document.createElement('li');
+                    const divTagForLaw = document.createElement("div");
+                    liTagForRecipe.setAttribute("id", `${singleRecipeObject.id}`)
+                    // console.log(singleRecipeObject.name)
+                    liTagForRecipe.innerText += singleRecipeObject.name
+                    divTagForLaw.append(liTagForRecipe)
+                    document.getElementById(`${law.id}`).append(divTagForLaw)
+                    // divTagForLaw.append(singleRecipeObject)
+                })
+            })
+}
+
+
+function moreRecipeInfoFetch(lawId){
+    fetch(`http://localhost:3000/laws/${lawId}`)
+            .then(resp => resp.json())
+            .then(law => {
+                law.recipes.forEach(singleRecipeObject => {
+                    const liTagForRecipe = document.createElement('li');
+                    const divTagForLaw = document.createElement("div");
+                    liTagForRecipe.setAttribute("id", `${singleRecipeObject.id}`)
+                    // console.log(singleRecipeObject.name)
+                    liTagForRecipe.innerText += singleRecipeObject.name
+                    divTagForLaw.append(liTagForRecipe)
+                    document.getElementById(`${law.id}`).append(divTagForLaw)
+                    // divTagForLaw.append(singleRecipeObject)
+                })
+            })
+}
+
 
 
 // fetching from my API for the laws
@@ -17,57 +70,10 @@ function renderLaws(dataLaw) {
             const br = document.createElement('br');
             divTagForLaw.innerText = law.name;
             const recipesForLaw = law.recipes
-            // console.log(recipesForLaw) //array of Recipe objects
-
-            //nested forEach to iterate through Recipe objects
-        //     recipesForLaw.forEach(singleRecipeObject => {
-        //         const liTagForRecipe = document.createElement('li');
-        //         liTagForRecipe.setAttribute("id", `${singleRecipeObject.id}`)
-        //         // console.log(singleRecipeObject.name)
-        //         liTagForRecipe.innerText += singleRecipeObject.name
-        //         divTagForLaw.append(liTagForRecipe)
-        //         // divTagForLaw.append(singleRecipeObject)
-        // })
-
-        ulLaws.append(br)
-        ulLaws.append(divTagForLaw)
-        
-        //create a function onClick to display recipes associated with each law
+            ulLaws.append(br)
+            ulLaws.append(divTagForLaw)
     });
 }
-
-
-
-
-document.addEventListener("click", () => {
-    handleClicks()
-})
-
-// document.addEventListener("DOMContentLoaded", function(){
-
-
-
-function handleClicks() {
-    // debugger
-    if (event.target.classList == "law" && event.target.id) {
-        let lawId = event.target.id;
-        const recipesForLaw = law.recipes
-        fetch(`http://localhost:3000/laws/${lawId}`)
-            .then(resp => resp.json())
-            .then(recipesForLaw.forEach(singleRecipeObject => {
-                const liTagForRecipe = document.createElement('li');
-                liTagForRecipe.setAttribute("id", `${singleRecipeObject.id}`)
-                // console.log(singleRecipeObject.name)
-                liTagForRecipe.innerText += singleRecipeObject.name
-                divTagForLaw.append(liTagForRecipe)
-                // divTagForLaw.append(singleRecipeObject)
-        }))
-    }
-    else if (event.target.id && event.target.classList == "recipe") { 
-        debugger
-    }
-}
-
 
 
 
