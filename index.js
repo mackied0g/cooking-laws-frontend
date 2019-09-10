0f92c0721a7cb4c98a3a5635ac6aefd66bdd4840
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-09-10 14:19:13 -0400

    ahh so ugly and many errors but I'm trying my best

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // const loginForm = document.querySelector('#div')
-const ulLaws = document.querySelector("#ul-of-laws");
-
-document.addEventListener("DOMContentLoaded", function(){
+const ulLaws = document.querySelector(".ul-of-laws");
 
 
 // fetching from my API for the laws
@@ -14,22 +12,25 @@ function renderLaws(dataLaw) {
     // console.log(dataLaw) //each dataLaw has nested array of recipes
        dataLaw.forEach(function(law) {
             const divTagForLaw = document.createElement("div");
+            divTagForLaw.setAttribute("class", "law")
+            divTagForLaw.setAttribute("id", `${law.id}`)
             const br = document.createElement('br');
             divTagForLaw.innerText = law.name;
             const recipesForLaw = law.recipes
-            console.log(recipesForLaw) //array of Recipe objects
+            // console.log(recipesForLaw) //array of Recipe objects
 
             //nested forEach to iterate through Recipe objects
-            recipesForLaw.forEach(singleRecipeObject => {
-                const liTagForRecipe = document.createElement('li');
-                liTagForRecipe.innerText += singleRecipeObject.name
-                divTagForLaw.append(liTagForRecipe)
-                divTagForLaw.append(singleRecipeObject)
-        })
-        
+        //     recipesForLaw.forEach(singleRecipeObject => {
+        //         const liTagForRecipe = document.createElement('li');
+        //         liTagForRecipe.setAttribute("id", `${singleRecipeObject.id}`)
+        //         // console.log(singleRecipeObject.name)
+        //         liTagForRecipe.innerText += singleRecipeObject.name
+        //         divTagForLaw.append(liTagForRecipe)
+        //         // divTagForLaw.append(singleRecipeObject)
+        // })
+
         ulLaws.append(br)
         ulLaws.append(divTagForLaw)
-        
         
         //create a function onClick to display recipes associated with each law
     });
@@ -37,5 +38,38 @@ function renderLaws(dataLaw) {
 
 
 
-// DOMContentLoaded
+
+document.addEventListener("click", () => {
+    handleClicks()
 })
+
+// document.addEventListener("DOMContentLoaded", function(){
+
+
+
+function handleClicks() {
+    // debugger
+    if (event.target.classList == "law" && event.target.id) {
+        let lawId = event.target.id;
+        const recipesForLaw = law.recipes
+        fetch(`http://localhost:3000/laws/${lawId}`)
+            .then(resp => resp.json())
+            .then(recipesForLaw.forEach(singleRecipeObject => {
+                const liTagForRecipe = document.createElement('li');
+                liTagForRecipe.setAttribute("id", `${singleRecipeObject.id}`)
+                // console.log(singleRecipeObject.name)
+                liTagForRecipe.innerText += singleRecipeObject.name
+                divTagForLaw.append(liTagForRecipe)
+                // divTagForLaw.append(singleRecipeObject)
+        }))
+    }
+    else if (event.target.id && event.target.classList == "recipe") { 
+        debugger
+    }
+}
+
+
+
+
+// DOMContentLoaded
+// })
