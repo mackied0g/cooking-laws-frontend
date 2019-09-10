4337641b8c41102fa4bf5f7f6183f7ed36bb9bb7
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-09-10 06:29:59 -0400

    updated organization of js and html

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+const loginForm = document.querySelector('#div')
+
+// fetching from my API for the laws
+
 fetch(`http://localhost:3000/laws`)
     .then(resp => resp.json())
     .then(data => renderLaws(data))
 
-    const ul = document.createElement("ul");
-    document.body.append(ul);
+const ul = document.querySelector("#ul-of-laws");
+    // document.body.append(ul);
+
+// taking the fetch request and rendering it to the DOM
 function renderLaws(data) {
     data.forEach(function(law) {
-        
         const li = document.createElement("li");
         li.innerHTML = law.name;
-        
-    ul.append(li)
+        ul.append(li)
     });
 }
+
