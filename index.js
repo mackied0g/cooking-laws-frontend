7e2a5cd14cd890d8024d4566ca1bbadf75f7ebec
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-09-11 06:10:54 -0400

    as I'm doing this, I'm thinking about whether I should even have a so-called login page or even have users create an account to create favorites

--- index.js
+++ index.js
@@ -20,67 +20,65 @@ function handleClicks() {
 
 function lawRecipeFetch(lawId){
     fetch(`http://localhost:3000/laws/${lawId}`)
-            .then(resp => resp.json())
-            .then(law => {
-                law.recipes.forEach(singleRecipeObject => {
-                    const liTagForRecipe = document.createElement('li');
-                    const divTagForLaw = document.createElement("div");
-                    liTagForRecipe.setAttribute("id", `${singleRecipeObject.id}`)
-                    liTagForRecipe.setAttribute("class", `recipe`)
-                    liTagForRecipe.innerText += singleRecipeObject.name
-                    divTagForLaw.append(liTagForRecipe)
-                    document.getElementById(`${law.id}`).append(divTagForLaw)
-                    // divTagForLaw.append(singleRecipeObject)
-                })
+        .then(resp => resp.json())
+        .then(law => {
+            law.recipes.forEach(singleRecipeObject => {
+                const liTagForRecipe = document.createElement('li');
+                const divTagForLaw = document.createElement("div");
+                liTagForRecipe.setAttribute("id", `${singleRecipeObject.id}`)
+                liTagForRecipe.setAttribute("class", `recipe`)
+                liTagForRecipe.innerText += singleRecipeObject.name
+                divTagForLaw.append(liTagForRecipe)
+                document.getElementById(`${law.id}`).append(divTagForLaw)
+                // divTagForLaw.append(singleRecipeObject)
             })
+        })
 }
 
 
 function moreRecipeInfoFetch(lawId, recipeId){
     fetch(`http://localhost:3000/laws/${lawId}`)
-            .then(resp => resp.json())
-            .then(lawObject => {
-                let recipeOfLawObject = lawObject.recipes
-             
-                recipeOfLawObject.forEach((obj) => {
+        .then(resp => resp.json())
+        .then(lawObject => {
+            let recipeOfLawObject = lawObject.recipes
+            recipeOfLawObject.forEach((obj) => {
+            
+            if(obj.id == recipeId ){
+                let recipe = obj
+                const pTagForRecipe = document.createElement('p');
+                const divTagForRecipe = document.createElement("div");
+                divTagForRecipe.dataset.id = recipe.id
+
+                pTagForDescription = document.createElement("p");
+                pTagForDescription.innerText = recipe.description
+                pTagForDescription.setAttribute("class", `pTagForDescription`)
+                // pTagForRecipe.innerText += recipe.description
+
+                pTagForYield = document.createElement("p");
+                pTagForYield.innerText = recipe.yield 
+                pTagForYield.setAttribute("class", `pTagForYield`)
+                // pTagForRecipe.innerText += recipe.yield
+
+                pTagForIngredients = document.createElement("p");
+                pTagForIngredients.innerText = recipe.ingredients
+                pTagForIngredients.setAttribute("class", `pTagForIngredients`)
+                // pTagForRecipe.innerText += recipe.ingredients
+
+                pTagForInstructions = document.createElement("p");
+                pTagForInstructions.innerText = recipe.instructions
+                pTagForInstructions.setAttribute("class", `pTagForInstructions`)
+                // pTagForRecipe.innerText += recipe.instructions
+
+                divTagForRecipe.append(pTagForRecipe)
+                document.getElementById(`${recipeId}`).append(divTagForRecipe)
                     
-                    if(obj.id == recipeId ){
-                        let recipe = obj
-                        const liForRecipe = document.createElement('li');
-                        const br = document.createElement('br');
-                        const divTagForRecipe = document.createElement("div");
-                        divTagForRecipe.dataset.id = recipe.id
-
-                        pTagForDescription = document.createElement("p");
-                        pTagForDescription.innerText = recipe.description
-                        pTagForDescription.setAttribute("class", `pTagForDescription`)
-                        // liForRecipe.innerText += recipe.description
-
-                        pTagForYield = document.createElement("p");
-                        pTagForYield.innerText = recipe.yield 
-                        pTagForYield.setAttribute("class", `pTagForYield`)
-                        // liForRecipe.innerText += recipe.yield
-
-                        pTagForIngredients = document.createElement("p");
-                        pTagForIngredients.innerText = recipe.ingredients
-                        pTagForIngredients.setAttribute("class", `pTagForIngredients`)
-                        // liForRecipe.innerText += recipe.ingredients
-
-                        pTagForInstructions = document.createElement("p");
-                        pTagForInstructions.innerText = recipe.instructions
-                        pTagForInstructions.setAttribute("class", `pTagForInstructions`)
-                        // liForRecipe.innerText += recipe.instructions
-
-                        divTagForRecipe.append(liForRecipe)
-                        document.getElementById(`${recipeId}`).append(divTagForRecipe)
-                    
-                        liForRecipe.appendChild(pTagForDescription)
-                        liForRecipe.appendChild(pTagForIngredients)
-                        liForRecipe.appendChild(pTagForInstructions)
-                        liForRecipe.appendChild(pTagForYield)
-                    }
-                  })
-            })
+                pTagForRecipe.appendChild(pTagForDescription)
+                pTagForRecipe.appendChild(pTagForIngredients)
+                pTagForRecipe.appendChild(pTagForInstructions)
+                pTagForRecipe.appendChild(pTagForYield)
+            }
+        })
+    })
 }
 
 
@@ -92,15 +90,30 @@ fetch(`http://localhost:3000/laws`)
 
 // taking the fetch request and rendering it to the DOM
 function renderLaws(dataLaw) {
-    // console.log(dataLaw) //each dataLaw has nested array of recipes
-       dataLaw.forEach(function(law) {
-            const divTagForLaw = document.createElement("div");
-            divTagForLaw.setAttribute("class", "law")
-            divTagForLaw.setAttribute("id", `${law.id}`)
-            const br = document.createElement('br');
-            divTagForLaw.innerText = law.name;
-            const recipesForLaw = law.recipes
-            ulLaws.append(br)
-            ulLaws.append(divTagForLaw)
+    dataLaw.forEach(function(law) {
+        const divTagForLaw = document.createElement("div");
+        divTagForLaw.setAttribute("class", "law")
+        divTagForLaw.setAttribute("id", `${law.id}`)
+        const br = document.createElement('br');
+        divTagForLaw.innerText = law.name;
+        const recipesForLaw = law.recipes
+        ulLaws.append(br)
+        ulLaws.append(divTagForLaw)
     });
+}
+
+
+
+
+function fetchUsers(){
+    fetch(`http://localhost:3000/users`)
+    .then(resp => resp.json())
+    .then(login)
+}
+
+function login(user){
+    const ul = document.createElement("ul")
+    const li = document.createElement("li");
+    li.innerHTML = ``
+    ul.append(li)
 }
